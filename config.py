eabcb07e393013143196f76f3578dc501566e13e
principle105 <principle105@gmail.com>
2023-01-06 22:21:12 -0500

    added discord oauth

--- config.py
+++ config.py
@@ -5,5 +5,13 @@ DATABASE_URI = config("DATABASE_URI")
 DATABASE_NAME = config("DATABASE_NAME")
 REDIS_URL = config("REDIS_URL")
 
+# Tokens
 DBL_TOKEN = config("DBL_TOKEN", default=None)
 KOFI_TOKEN = config("KOFI_TOKEN", default=None)
+
+# Discord OAUTH
+CLIENT_ID = config("CLIENT_ID")
+CLIENT_SECRET = config("CLIENT_SECRET")
+ALGORITHM = config("ALGORITHM")
+REDIRECT_URL = config("REDIRECT_URL")
+SECRET = config("SECRET")
--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 import json
 from datetime import datetime
 
+import jwt
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from motor.motor_asyncio import AsyncIOMotorClient
 from redis import asyncio as aioredis
+from starlette_discord import DiscordOAuthClient
 
-from config import DATABASE_NAME, DATABASE_URI, DBL_TOKEN, KOFI_TOKEN, REDIS_URL
+from config import (
+    ALGORITHM,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    DATABASE_NAME,
+    DATABASE_URI,
+    DBL_TOKEN,
+    KOFI_TOKEN,
+    REDIRECT_URL,
+    REDIS_URL,
+    SECRET,
+)
 
 app = FastAPI()
+
+discord = DiscordOAuthClient(
+    CLIENT_ID, CLIENT_SECRET, REDIRECT_URL, ("identify", "email")
+)
+
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[REDIRECT_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Databases
 client = AsyncIOMotorClient(DATABASE_URI)
@@ -25,12 +53,6 @@ def verify_dbl_auth(req: Request):
     return req
 
 
-def get_data_from_form(form_data):
-    data = str(form_data.get("data", None))
-
-    return json.loads(data)
-
-
 async def verify_kofi_auth(req: Request):
     form_data = await req.form()
 
@@ -45,9 +67,15 @@ async def verify_kofi_auth(req: Request):
     return req
 
 
+def get_data_from_form(form_data):
+    data = str(form_data.get("data", None))
+
+    return json.loads(data)
+
+
 @app.on_event("startup")
 async def startup_event():
-    global redis
+    global redis  # thomas doesn't like globals :(
     redis = await aioredis.from_url(REDIS_URL, socket_timeout=10, max_connections=2)
 
 
@@ -70,7 +98,7 @@ async def premium(request: Request = Depends(verify_kofi_auth)):
 
     await db.premium.insert_one(
         {
-            "_id": "c1b6754f-b43d-454b-97b2-423876716273",
+            "_id": data["message_id"],
             "email": data["email"],
             "name": data["from_name"],
             "tier": data["tier_name"],
@@ -117,4 +145,45 @@ async def vote(request: Request = Depends(verify_dbl_auth)):
     return "Thomas is happy!"
 
 
-uvicorn.run(app, host="0.0.0.0", port=8080)
+@app.get("/login")
+async def start_login():
+    return discord.redirect()
+
+
+@app.get("/token")
+async def callback(code: str):
+    user = await discord.login(code)
+
+    payload = {
+        "id": user.id,
+    }
+
+    jwt_token = jwt.encode(payload, SECRET, algorithm=ALGORITHM)
+
+    return JSONResponse({"token": jwt_token})
+
+
+@app.get("/user")
+async def get_user(token: str):
+    try:
+        payload = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
+    except jwt.InvalidTokenError:
+        raise HTTPException(401)
+
+    user = await db.users.find_one({"_id": payload["id"]})
+
+    if user is None:
+        raise HTTPException(404)
+
+    user_data = {
+        "id": str(user["_id"]),
+        "name": user["name"],
+        "discriminator": user["discriminator"],
+        "avatar": user["avatar"],
+    }
+
+    return JSONResponse(user_data)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
